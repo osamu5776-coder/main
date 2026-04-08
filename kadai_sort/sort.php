643269d390f26aec1d1b595e49b802b1ba2bbb00
osamu5776-coder <osamu5776@gmail.com>
2026-04-08 19:26:17 +0900

    kadai_sortを追加

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -8,38 +8,32 @@
 
      <body>
            <p>
-               <?php
-               // ソートする配列を宣言
-               $nums = [15, 4, 18, 23, 10];
-              
-               // ソート関数
-               function sort_2way(array &$array, bool $order): void {
-               if ($order === true) {
-               // 昇順
-               sort($array);
-               } else {
-               // 降順
-               rsort($array);
-               }
+              <?php
+              // ソート関数
+              function sort_2way(array $array, bool $order): void {
+
+              if ($order) {
+              echo "昇順にソートします。<br>";
+              sort($array);
+              } else {
+              echo "降順にソートします。<br>";
+              rsort($array);
               }
 
-              // 昇順ソート
-              echo "昇順にソートします。<br>";
+              // foreachで出力
+              foreach ($array as $num) {
+              echo $num . "<br>";
+              }
+             }
+
+              // ソートする配列
+              $nums = [15, 4, 18, 23, 10];
+
+              // 関数呼び出し（昇順）
               sort_2way($nums, true);
 
-              // foreachで出力
-              foreach ($nums as $num) {
-              echo $num . "<br>";
-              }
-
-              // 降順ソート
-              echo "降順にソートします。<br>";
+              // 関数呼び出し（降順）
               sort_2way($nums, false);
-
-              // foreachで出力
-              foreach ($nums as $num) {
-              echo $num . "<br>";
-              }
               ?>
           </p>
     </body>
